be9ebdca8d9cb000e07827978c5cc513172b9bf7
DACHMAR <163539310+DACHMAR@users.noreply.github.com>
2024-05-05 15:55:09 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# AGENCECO-Site-WEB
---
+++ script.js
@@ -0,0 +1,28 @@
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+  hamburgerButton.classList.toggle("active")
+  navigation.classList.toggle("active")
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
